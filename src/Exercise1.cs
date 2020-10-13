72cb5e5abed2a86fcbc0feb65855d7f78f3168c8
Bogdan Polak <bogdanpol@gmail.com>
2020-10-13 22:58:50 +0200

    _formatted

--- src/Exercise1.cs
+++ src/Exercise1.cs
@@ -1,28 +1,35 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
-namespace CodeTest {
-    
-    public class Foo 
+namespace CodeTest
+{
+
+    public class Foo
     {
-        public int No { get; private set; } 
+        public int No { get; private set; }
         public string Text { get; private set; }
-        public Foo() {}
-        public Foo(int no, string text)
+        public string Country { get; private set; }
+        public Foo() { }
+        public Foo(int no, string text, string country)
         {
             No = no;
             Text = text;
+            Country = country;
         }
+        public override string ToString() => $"{No}={Text} ({Country})";
     }
-    
+
     public class Exercise1
     {
-        public void Run() {
+        public void Run()
+        {
             var list = new List<Foo>() {
-                new Foo(1, "Sylvester"),
-                new Foo(5, "Bogdan"),
-                new Foo(3, "Daniel")
+                new Foo(1, "Sylvester", "UK"),
+                new Foo(7, "Bogdan", "Poland"),
+                new Foo(3, "Daniel", "UK"),
+                new Foo(2, "Robert", "Poland")
             };
             var length = list.Count;
             var date = DateTime.Now;
