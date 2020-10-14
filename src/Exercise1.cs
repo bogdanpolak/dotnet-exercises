0ee2cf03aac9a3cbdceb2a2902f90903750c8f59
Bogdan Polak <bogdanpol@gmail.com>
2020-10-14 20:07:50 +0200

    Linq Sum, Average, Max

--- src/Exercise1.cs
+++ src/Exercise1.cs
@@ -11,12 +11,20 @@ namespace CodeTest
         public int NO { get; private set; }
         public string Name { get; private set; }
         public string Country { get; private set; }
+        public Nullable<decimal> Salary { get; private set; }
         public Buddy() { }
         public Buddy(int no, string text, string country)
         {
             NO = no;
             Name = text;
             Country = country;
+        }
+        public Buddy(int no, string text, string country, decimal salary)
+        {
+            NO = no;
+            Name = text;
+            Country = country;
+            Salary = salary;
         }
         public override string ToString() => $"{NO}={Name} ({Country})";
     }
@@ -27,11 +35,11 @@ namespace CodeTest
         {
             var buddiesAll = new List<Buddy>() {
                 new Buddy(1, "Sylvester", "UK"),
-                new Buddy(7, "Bogdan", "Poland"),
-                new Buddy(3, "Mark", "UK"),
-                new Buddy(2, "Robert", "Poland"),
-                new Buddy(6, "Akos", "UK"),
-                new Buddy(9, "Sergey", "Ukraine")
+                new Buddy(7, "Bogdan", "Poland", 51000),
+                new Buddy(3, "Mark", "UK", 65000),
+                new Buddy(2, "Robert", "Poland", 43000),
+                new Buddy(6, "Akos", "UK", 49000),
+                new Buddy(9, "Sergey", "Ukraine", 34000)
             };
             ConsoleTools.WriteCollection(buddiesAll);
             // [1=Sylvester (UK), 7=Bogdan (Poland), 3=Mark (UK), 2=Robert (Poland), 6=Akos (UK), 9=Sergey (Ukraine)]
@@ -85,8 +93,17 @@ namespace CodeTest
             Console.WriteLine(msg);
             // I'm a polyglot
 
+            // -- Linq Sum, Average, Max ---------------------------
+            var total = buddiesAll.Where(buddy=>buddy.Salary.HasValue)
+                .Sum(buddy=>buddy.Salary.Value);
+            var avg = buddiesAll.Where(buddy=>buddy.Salary.HasValue)
+                .Average(buddy=>buddy.Salary.Value);
+            var max = buddiesAll.Where(buddy=>buddy.Salary.HasValue)
+                .Max(buddy=>buddy.Salary.Value);
+            Console.WriteLine($"Total: {total:n0}  Average: {avg:n0}  Maximum: {max:n0}");
+            // Total: 242,000  Average: 48,400  Maximum: 65,000
+   
 
-            // TODO: Max / Average / Sum
             // TODO: OrderByDescending (Country) ThenBy (Name)
             // TODO: Intersect https://docs.microsoft.com/pl-pl/dotnet/api/system.linq.enumerable.intersect?view=netcore-3.1
         }
