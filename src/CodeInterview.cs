fb8811593e3cb5882dbd17714d2b07782468b122
Bogdan Polak <bogdanpol@gmail.com>
2020-10-16 09:44:18 +0200

    Added CodeInterview class

---
+++ src/CodeInterview.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeTest
+{
+    public class CodeInterview
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -6,9 +6,12 @@ namespace CodeTest
     {
         static void Main(string[] args)
         {
+            /*
             new Exercise1().Run();
             Console.WriteLine("-----------------");
             new Exercise2().Run();
+            */
+            new CodeInterview().Run();
         }
     }
 }
