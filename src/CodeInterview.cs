176ed4230d7b8a9bfc0c303ee357614134e84a4c
Bogdan Polak <bogdanpol@gmail.com>
2020-10-16 19:00:01 +0200

    Added review task

--- src/CodeInterview.cs
+++ src/CodeInterview.cs
@@ -7,6 +7,23 @@ namespace CodeTest
 {
     public class CodeInterview
     {
+        /* ===== Expected output ===== */
+        // Chef's menu:
+        // STARTERS
+        // tartare or pancake
+        // MAINS
+        // risotto, salmon or lamb
+        // DESERTS
+        // icecream
+
+        Dictionary<string, List<string>> MENU = new Dictionary<string, List<string>> {
+            { "starters", new List<string>(){ "tartare", "pancake", "salad" } },
+            { "mains", new List<string>(){ "risotto", "lamb", "haddock", "salmon", "steak" } },
+            { "deserts", new List<string>(){ "sorbet", "icecream", "cheesecake" } }
+        };
+        List<string> CHEFS_DISHES = new List<string>() {
+            "tartare","risotto","salmon","icecream","pancake","lamb"};
+
         public void Run()
         {
             Console.WriteLine("Hello");
