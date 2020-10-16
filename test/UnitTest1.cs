01f3f1a5673804da8bdb16be3765bdd6d0fd4f81
Bogdan Polak <bogdanpol@gmail.com>
2020-10-16 23:43:00 +0200

    Add tests

---
+++ test/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using DotNetExercises;
+
+namespace TestExercises2
+{
+    public class CodeInterviewTest
+    {
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("invalid", "")]
+        [InlineData("starters", "tartare or pancake")]
+        [InlineData("mains", "risotto, lamb or salmon")]
+        [InlineData("desserts", "icecream")]
+        public void GenerateServed(string category, string expected)
+        {
+            var codeInterview = new CodeInterview();
+            var actual = codeInterview.GenerateServed(category);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
