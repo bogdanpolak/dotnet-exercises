fd0e2c7ac699f0fa18d3d5899a244ca087b6bd1d
Bogdan Polak <bogdanpol@gmail.com>
2020-10-16 21:01:34 +0200

    Comleted solution

--- src/CodeInterview.cs
+++ src/CodeInterview.cs
@@ -24,9 +24,30 @@ namespace CodeTest
         List<string> CHEFS_DISHES = new List<string>() {
             "tartare","risotto","salmon","icecream","pancake","lamb"};
 
+        public string GenerateServed(string category)
+        {
+            var list = MENU[category].Intersect(CHEFS_DISHES).ToList();
+            var sb = new StringBuilder();
+            for (var i = 0; i < list.Count; i++) {
+                if (i>0 && i == list.Count - 1)
+                    sb.Append(" or ");
+                else if (i > 0)
+                    sb.Append(", ");
+                sb.Append(list[i]);
+            }
+            return sb.ToString();
+        }
+
         public void Run()
         {
-            Console.WriteLine("Hello");
+            var codeInterview = new CodeInterview();
+            Console.WriteLine("Chef's menu:");
+            Console.WriteLine("STARTERS");
+            Console.WriteLine(codeInterview.GenerateServed("starters"));
+            Console.WriteLine("MAINS");
+            Console.WriteLine(codeInterview.GenerateServed("mains"));
+            Console.WriteLine("DESSERTS");
+            Console.WriteLine(codeInterview.GenerateServed("desserts"));
         }
     }
 }
